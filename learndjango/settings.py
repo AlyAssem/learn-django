09c2e8dfca1b82dffff44ada9c429e9d1fa9c660
AlyAssem <alyassem1997@gmail.com>
2022-01-16 17:01:01 +0300

    created a component with a simple model and configured it in settings for installation

--- learndjango/settings.py
+++ learndjango/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # install component created locally
+    'articles'
 ]
 
 MIDDLEWARE = [
