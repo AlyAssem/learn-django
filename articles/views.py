a877ded9921e3c839d9d32f63bb426cab8fa5e2e
AlyAssem <alyassem1997@gmail.com>
2022-01-20 19:44:08 +0300

    customized admin page list view & setup a basic search for articles

--- articles/views.py
+++ articles/views.py
@@ -4,6 +4,24 @@ from articles.models import Article
 # Create your views here.
 
 
+def article_search_view(request):
+    query_dict = request.GET
+    try:
+        query = int(query_dict.get("q"))  # q is the name of the input field.
+    except:
+        query = None
+
+    article_object = None
+    if query is not None:
+        article_object = Article.objects.get(id=query)
+
+    context = {
+        "detail_object": article_object
+    }
+
+    return render(request, "articles/search.html", context)
+
+
 def article_detail_view(request, id):
     if id is not None:
         article_detail_obj = Article.objects.get(id=id)
--- learndjango/urls.py
+++ learndjango/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import home_view
-from articles.views import article_detail_view
+from articles.views import article_detail_view, article_search_view
 
 urlpatterns = [
+    path('articles/', article_search_view),
     path('articles/<int:id>', article_detail_view),
     path('', home_view),
     path('admin/', admin.site.urls),
